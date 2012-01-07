13ff51e7f044600670907c4e3b5417613304d7ec
Michael Garriss <mgarriss@gmail.com>
2012-01-07 10:12:31 -0800

    rename integer_helper.rb

--- lib/peas/integer_helper.rb
+++ lib/peas/integer_helper.rb
@@ -2,42 +2,42 @@ require 'peas/interval'
 
 class Integer
   def unisons
-    Peas::Interval::Diatonic::P1
+    Peas::Interval::P1
   end
   alias unison unisons
   
   def semitones
-    Peas::Interval::Semitone "S#{self}" => self
+    Peas::Interval "S#{self}" => self
   end
   alias semitone semitones
   
   def minor_seconds
-    Peas::Interval::Diatonic "m2x#{self}" => self
+    Peas::Interval "m2x#{self}" => self
   end
   alias minor_second minor_seconds
   
   def wholetones
-    Peas::Interval::Wholetone "W#{self}" => self * 2
+    Peas::Interval "W#{self}" => self * 2
   end
   alias wholetone wholetones
   
   def major_seconds
-    Peas::Interval::Diatonic "M2x#{self}" => self * 2
+    Peas::Interval "M2x#{self}" => self * 2
   end
   alias major_second major_seconds
   
   def minor_thirds
-    Peas::Interval::Diatonic "m3x#{self}" => self * 3
+    Peas::Interval "m3x#{self}" => self * 3
   end
   alias minor_third minor_thirds
   
   def major_thirds
-    Peas::Interval::Diatonic "M3x#{self}" => self * 4
+    Peas::Interval "M3x#{self}" => self * 4
   end
   alias major_third major_thirds
   
   def perfect_fourths
-    Peas::Interval::Diatonic "P4x#{self}" => self * 5
+    Peas::Interval "P4x#{self}" => self * 5
   end
   alias perfect_fourth perfect_fourths
   
@@ -48,47 +48,47 @@ class Integer
   alias tritone tritones
   
   def diminished_fifths
-    Peas::Interval::Diatonic "d5x#{self}" => self * 6
+    Peas::Interval "d5x#{self}" => self * 6
   end
   alias diminished_fifth diminished_fifths
   
   def augmented_fourths
-    Peas::Interval::Diatonic "S6x#{self}" => self * 6
+    Peas::Interval "S6x#{self}" => self * 6
   end
   alias augmented_fourth augmented_fourths
   
   def perfect_fifths
-    Peas::Interval::Diatonic "P5x#{self}" => self * 7
+    Peas::Interval "P5x#{self}" => self * 7
   end
   alias perfect_fifth perfect_fifths
   
   def minor_sixths
-    Peas::Interval::Diatonic "m6x#{self}" => self * 8
+    Peas::Interval "m6x#{self}" => self * 8
   end
   alias minor_sixth minor_sixths
   
   def augmented_fifths
-    Peas::Interval::Chromatic "S8x#{self}" => self * 8
+    Peas::Interval "S8x#{self}" => self * 8
   end
   alias augmented_fifth augmented_fifths
   
   def major_sixths
-    Peas::Interval::Diatonic "M6x#{self}" => self * 9
+    Peas::Interval "M6x#{self}" => self * 9
   end
   alias major_sixth major_sixths
   
   def minor_sevenths
-    Peas::Interval::Diatonic "m7x#{self}" => self * 10
+    Peas::Interval "m7x#{self}" => self * 10
   end
   alias minor_seventh minor_sevenths
   
   def major_sevenths
-    Peas::Interval::Diatonic "M7x#{self}" => self * 11
+    Peas::Interval "M7x#{self}" => self * 11
   end
   alias major_seventh major_sevenths
   
   def octaves
-    Peas::Interval::Diatonic "P8x#{self}" => self * 12
+    Peas::Interval "P8x#{self}" => self * 12
   end
   alias octave octaves
 end
